485bebdec28e8e6a1f6a76ce6535b68b75bb8089
KLBeneteau <kael@ouivalo.fr>
2022-12-29 13:55:29 +0100

    création du plugins md5

---
+++ md5/README.md
@@ -0,0 +1,20 @@
+# Md5
+This is a readme for your new Budibase plugin.
+
+# Description
+Generate your secret token
+
+Find out more about [Budibase](https://github.com/Budibase/budibase).
+
+## Instructions
+
+To build your new  plugin run the following in your Budibase CLI:
+```
+budi plugins --build
+```
+
+You can also re-build everytime you make a change to your plugin with the command:
+```
+budi plugins --watch
+```
+
---
+++ md5/src/Component.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { getContext } from "svelte"
+  import md5 from "md5"
+
+  export let secret
+
+  const { styleable, Provider  } = getContext("sdk")
+  const component = getContext("component")
+
+  let token = md5('ouivalo'+secret+Math.floor(Math.random() * 10000)).toString();
+  $: dataContext = {
+    token
+  }
+</script>
+
+<div use:styleable={$component.styles}>
+  <Provider data={dataContext}>
+    <slot />
+  </Provider>
+</div>
