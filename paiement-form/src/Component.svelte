c5b685704b0d0df95fcf79ee20d18876cc80ef6b
User <kael.beneteau@gmail.com>
2023-01-06 11:47:55 +0100

    avancé sur le formulaire

--- paiement-form/src/Component.svelte
+++ paiement-form/src/Component.svelte
@@ -5,23 +5,32 @@
   const { styleable } = getContext("sdk")
   const component = getContext("component")
 
+  export let vads_trans_id
+  export let vads_order_id
+
   //mdp carte : 1356
+  function complete(value){
+    return value < 10 ? '0'+value : value.toString()
+  }
+
+  let now = new Date(Date.now())
+  const AAAAMMJJHHMMSS = now.getUTCFullYear()+complete(now.getUTCMonth()+1)+complete(now.getUTCDate())+complete(now.getUTCHours())+complete(now.getUTCMinutes())+complete(now.getUTCSeconds())
   const data = {
     vads_action_mode : 'INTERACTIVE',
-    vads_amount : 5124, //100
-    //vads_capture_delay : 0,
-    vads_ctx_mode : "TEST", //"PRODUCTION"
+    vads_amount : 100,
+    vads_capture_delay : 0,
+    vads_ctx_mode : "PRODUCTION",
     vads_currency : 978,
-    //vads_order_id : "CX-1254",
+    vads_order_id : vads_order_id,
     vads_page_action : "PAYMENT",
-    //vads_payment_cards : "CB",
+    vads_payment_cards : "CB",
     vads_payment_config : "SINGLE",
-    vads_site_id : 12345678, //29747086
-    vads_trans_date : 20170129130025,
-    vads_trans_id : "123456",
+    vads_site_id : 29747086,
+    vads_trans_date : AAAAMMJJHHMMSS,
+    vads_trans_id : vads_trans_id,
     vads_version : "V2",
   }
-  const secret = "1122334455667788" //'o7rMBE2Z2UgVWmxE6iLjzc9xS1RCzCS7L7IZqtitGJg47'
+  const secret = 'LyOuEbpRWMuYHF2l'
 
   let message = ""
   let datakeys = Object.getOwnPropertyNames(data).sort()
@@ -34,6 +43,10 @@
   let secret8 = CryptoJS.enc.Utf8.parse(secret)
 
   const signature = CryptoJS.enc.Base64.stringify(CryptoJS.HmacSHA256(message8,secret8))
+
+  console.log(message,message8)
+  console.log(secret,secret8)
+  console.log(signature)
   
 </script>
 
@@ -45,19 +58,19 @@
     src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
   </iframe>
   <form method="POST" action="https://sogecommerce.societegenerale.eu/vads-payment/">
-    <input type="hidden" name="vads_action_mode" value="INTERACTIVE" />
-    <input type="hidden" name="vads_amount" value="100" />
-    <input type="hidden" name="vads_capture_delay" value="0" />
-    <input type="hidden" name="vads_ctx_mode" value="PRODUCTION" />
-    <input type="hidden" name="vads_currency" value="978" />
-    <input type="hidden" name="vads_order_id" value="CX-1254" />
-    <input type="hidden" name="vads_page_action" value="PAYMENT" />
-    <input type="hidden" name="vads_payment_cards" value="CB" />
-    <input type="hidden" name="vads_payment_config" value="SINGLE" />
-    <input type="hidden" name="vads_site_id" value="29747086" />
-    <input type="hidden" name="vads_trans_date" value="20190626101407" />
-    <input type="hidden" name="vads_trans_id" value="pt156G" />
-    <input type="hidden" name="vads_version" value="V2" />
+    <input type="hidden" name="vads_action_mode" value="{data.vads_action_mode}" />
+    <input type="hidden" name="vads_amount" value="{data.vads_amount}" />
+    <input type="hidden" name="vads_capture_delay" value="{data.vads_capture_delay}" />
+    <input type="hidden" name="vads_ctx_mode" value="{data.vads_ctx_mode}" />
+    <input type="hidden" name="vads_currency" value="{data.vads_currency}" />
+    <input type="hidden" name="vads_order_id" value="{data.vads_order_id}" />
+    <input type="hidden" name="vads_page_action" value="{data.vads_page_action}" />
+    <input type="hidden" name="vads_payment_cards" value="{data.vads_payment_cards}" />
+    <input type="hidden" name="vads_payment_config" value="{data.vads_payment_config}" />
+    <input type="hidden" name="vads_site_id" value="{data.vads_site_id}" />
+    <input type="hidden" name="vads_trans_date" value="{data.vads_trans_date}" />
+    <input type="hidden" name="vads_trans_id" value="{data.vads_trans_id}" />
+    <input type="hidden" name="vads_version" value="{data.vads_version}" />
     <input type="hidden" name="signature" value="{signature}"/>
     <input type="submit" name="payer" value="Payer"/>
   </form>
